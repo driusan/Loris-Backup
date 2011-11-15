e4f778c497b3a8478762751860c0b950e19ce144
Dave MacFarlane <driusan@gmail.com>
2011-11-15 12:17:44 -0500

    changed update_cbrain_status to use prod file and non-hardcoded parameters

--- tools/update_cbrain_status.pl
+++ tools/update_cbrain_status.pl
@@ -8,7 +8,7 @@
 
 require CbrainAPI;
 use strict;
-use DBI;
+use NeuroDB::DBI;
 use Getopt::Tabular;
 
 my $profile;
@@ -20,44 +20,91 @@ my @arg_table =
 );
 
 my $result = GetOptions(\@arg_table, \@ARGV);
+if(!defined($profile)) {
+    print "profile is required\n";
+    exit(-1);
+}
 
-# Magic values that Pierre asked me to use
-# They should be un-hardcoded once the API
-# allows it.
-my $ProviderID = 136;
-my $ToolConfigID = 7;
-my $ToolConfigID = 76;
-my $cbrainhost = 'http://bianca.cbrain.mcgill.ca:3000/';
-my $civetprefix = "ibis";
+# Group prepared statements together and throw in some helper functions to execute them
+# This is the only interaction we have directly with the DB
+{
+    { package Settings; do "$ENV{HOME}/.neurodb/$profile" }
+    if (!defined(@Settings::db)) {
+        print "\n\tERROR: You don't have a configuration file named '$profile' in:  $ENV{HOME}/.neurodb/ \n\n";
+        exit(33);
+    }
 
-# Username/password to login to cbrain with.
-# This should be converted into commandline
-# options. The password should be a 
-# hash once CBrain API allows it.
-my $UserName = "loris";
-my $Password = "qwer";
+    my $dbh = &NeuroDB::DBI::connect_to_db(@Settings::db);
+    my $configsth= $dbh->prepare("SELECT c.Value FROM ConfigSettings cs LEFT JOIN Config c ON (c.ConfigID=cs.ID) WHERE cs.Name=?");
+    my $updatetasksth = $dbh->prepare("INSERT INTO CBrainTasks (UserID, Timestamp, CBrainHost, TaskID, Status) VALUES (?, ?, ?, ?, ?)");
+    my $upsth = $dbh->prepare("UPDATE CBrainTasks SET Status=? WHERE TaskID=?");
+    my $compsth = $dbh->prepare("UPDATE CBrainTasks SET CompletedAt = now() WHERE TaskID=?");
+    my $incsth = $dbh->prepare("SELECT TaskID FROM CBrainTasks WHERE Status IN (?, ?, ?, ?, ?, ?, ?)");
+    sub _GetSingleVal {
+        my $sth = shift;
+        my $param = shift;
+        $sth->execute($param);
+        my @results = $sth->fetchrow_array;
+        return $results[0];
+    }
+    sub GetConfigOption {
+        my $name = shift;
+        return _GetSingleVal($configsth, $name);
+    }
+    sub GetIncompleteTasks {
+        my @tasks;
+        $incsth->execute("Unknown", "New", "Setting Up", "Queued", "Data Ready", "On CPU",  "Post Processing");
+        while(my @row = $incsth->fetchrow_array) {
+            push(@tasks, $row[0]);
+        }
+        return @tasks;
+    }
+    sub UpdateTask {
+        my $TaskID = shift;
+        my $Status = shift;
+        $upsth->execute($Status, $TaskID);
+    }
+    sub CompleteTask {
+        my $TaskID = shift;
+        $compsth->execute($TaskID);
+    }
+    sub DisconnectDBH {
+        $configsth->finish;
+        $updatetasksth->finish;
+        $upsth->finish;
+        $compsth->finish;
+        $incsth->finish;
+        
+        $dbh->disconnect();
+    }
+}
 
-my $dbh = DBI->connect("DBI:mysql:database=loris;host=localhost;port=3306", "root", "abc123!");
+my $ProviderID = GetConfigOption("CBrainProviderID");
+my $ToolConfigID = GetConfigOption("CBrainToolConfigID");;
+my $cbrainhost = GetConfigOption("CBrainHost");
+my $civetprefix = "civet";
 
-my $sth = $dbh->prepare("SELECT TaskID FROM CBrainTasks WHERE Status IN (?, ?, ?, ?, ?, ?, ?)");
-$sth->execute("Unknown", "New", "Setting Up", "Queued", "Data Ready", "On CPU",  "Post Processing");
-my $upsth = $dbh->prepare("UPDATE CBrainTasks SET Status=? WHERE TaskID=?");
-my $compsth = $dbh->prepare("UPDATE CBrainTasks SET CompletedAt = now() WHERE TaskID=?");
+my $UserName = GetConfigOption("CBrainUsername");
+my $Password = GetConfigOption("CBrainPassword");
+
+my @tasks = GetIncompleteTasks();
+
 my $session = CbrainAPI->new(
     cbrain_server_url => $cbrainhost
 );
 $session->login($UserName, $Password);
 
-while(my @row = $sth->fetchrow_array) {
-    my $task = $session->show_task($row[0]);
+foreach my $taskid (@tasks) {
+    my $task = $session->show_task($taskid);
 
-    print "$row[0]: ";
+    print "$taskid: ";
     print $task->{'status'};
     if($task->{'status'} =~ m/Completed/) {
-        $compsth->execute($row[0]);
+        CompleteTask($taskid);
     }
     print "\n";
-    $upsth->execute($task->{'status'}, $row[0]);
+    UpdateTask($taskid, $task->{'status'});
 }
 
-$dbh->disconnect();
+DisconnectDBH();
+
