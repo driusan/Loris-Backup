ee0cfdcc4e0cf895f7d1f9b01b75ef6e43dd06cc
Dave MacFarlane <driusan@gmail.com>
2012-04-10 10:48:23 -0400

    Added ability to update instead of just create datadict

--- tools/couchdb_import_datadict.php
+++ tools/couchdb_import_datadict.php
@@ -9,27 +9,33 @@ $client->initialize();
 $config =& NDB_Config::singleton();
 $db = Database::singleton();
 
-$test = $db->pselect("select pt.SourceFrom, pt.SourceField, pt.Type, pt.Description from parameter_type pt LEFT JOIn parameter_type_category_rel ptcr USING (ParameterTypeID) LEFT JOIN parameter_type_category ptc USING (ParameterTypeCategoryID) WHERE pt.Queryable=1 AND ptc.Type='Instrument' AND SourceFrom IN ('tsi', 'rbs_r') ORDER BY SourceFrom", array());
-$couchdbName = 'loris_test_2';
+$test = $db->pselect("select pt.SourceFrom, pt.SourceField, pt.Type, pt.Description from parameter_type pt LEFT JOIN parameter_type_category_rel ptcr USING (ParameterTypeID) LEFT JOIN parameter_type_category ptc USING (ParameterTypeCategoryID) WHERE pt.Queryable=1 AND ptc.Type='Instrument' ORDER BY SourceFrom", array());
+$couchdbName = 'ibis';
 $putValue = array('Meta' => array('DataDict' => true));
 $Dictionary = array();
 $Instrument = array();
 $lastFrom = '';
 
+function PutVal($Dictionary, $Name, $stub) {
+    global $db, $couchdbName;
+    $old_value = $db->getCouch("DataDictionary:$Name", $couchdbName);
+    if($old_value['_rev']) {
+        $stub['_rev'] = $old_value['_rev'];
+    }
+    $stub['DataDictionary'] = array($Name => $Dictionary);
+    $db->putCouch("DataDictionary:$Name", $stub, $couchdbName);
+}
 
 foreach($test as $row) {
     $SourceFrom = $row['SourceFrom'];
     if($lastFrom != $SourceFrom) {
-        $Dictionary[$lastFrom] = $Instrument;
+        PutVal($Instrument, $lastFrom, $putValue);
         $Instrument = array();
     }
     $Instrument[$row['SourceField']] = array('Description' => $row['Description'], 'Type' => $row['Type']);
     $lastFrom = $row['SourceFrom'];
     
 }
-$Dictionary[$lastFrom] = $Instrument;
-$putValue['DataDictionary'] = $Dictionary;
 
-$db->putCouch("DataDictionary", $putValue, $couchdbName);
-
+PutVal($Instrument, $lastFrom, $putValue);
 ?>
