f2ce1e6736ee51d56ecf5fb559581f128a19a11c
Rathi Sekaran <sekaranrathi@gmail.com>
2014-02-20 01:32:59 -0500

    adding php file to run qc reports by site

---
+++ tools/qc_run.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../project/libraries/QC_Checks.class.inc";
+// define a config file to use
+$configFile = "../project/config.xml";
+
+//set_include_path(get_include_path().":../php/libraries:");
+set_include_path(get_include_path().":../project/libraries:");
+require_once "NDB_Client.class.inc";
+
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize($configFile);
+// get a Database connection
+$config =& NDB_Config::singleton();
+$dbConfig = $config->getSetting('database');
+$DB = new Database;
+$result = $DB->connect($dbConfig['database'], $dbConfig['username'], $dbConfig['password'], $dbConfig['host'], false);
+if(PEAR::isError($result)) {
+        die("Could not connect to database: ".$result->getMessage());
+}
+$sites =array('2','3','4','5'); 
+//print_r($sites);
+//$sites = array('2');
+foreach($sites as $site) {
+$run = new QC_Checks($site);
+ $run->run();
+}
+
+?>
